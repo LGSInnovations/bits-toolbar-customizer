65fbd5ba59fe1b0e798752f9a100d9c21f675c0e
Leone, Mark A [Axios] <mleone@lgsinnovations.com>
2018-07-13 12:03:20 -0400

    Persist customizations to file

--- bits-toolbar-customizer-manager.js
+++ bits-toolbar-customizer-manager.js
@@ -4,8 +4,12 @@
   const BitsToolbarCustomizerMessenger = require('./bits-toolbar-customizer-messenger');
 
   const {EVENTS} = require('./bits-toolbar-customizer-constants');
-  
+
+  const path = require('path');
+
   const SCOPES = {scopes: ['public']};
+
+  const UtilFs = global.utils.UtilFs;
 
   class BitsToolbarCustomizerManager {
 
@@ -16,13 +20,40 @@
     load(messageCenter) {
       this._messageCenter = messageCenter;
       this._messenger.load(messageCenter);
+      this._moduleApi = new global.helper.BaseModuleApi(messageCenter);
+      this.loadCustomizations();
     }
 
     setCustomizations(data) {
-      console.log(`set customization ${JSON.stringify(data, null, 2)}`);
       this._customizations = data.queries;
       this._hiddenProps = data.hiddenProps;
       this._messageCenter.sendEvent(EVENTS.UPDATE_CUSTOMIZATIONS, SCOPES, data);
+      this.dumpCustomizations(data);
+    }
+
+    dumpCustomizations(data) {
+      this._moduleApi.getDataDirectory('bits-toolbar-customizer')
+        .then( (dir) => {
+          return UtilFs.writeFile(path.resolve(dir, './customizations.json'), JSON.stringify(data, null, 2), {})
+            .catch((err) => {
+              console.log(`Failed to write toolbar customizations to file: ${err}`)
+            });
+        });
+    }
+
+    loadCustomizations() {
+      this._moduleApi.getDataDirectory('bits-toolbar-customizer')
+        .then( (dir) => {
+          return UtilFs.readFile(path.resolve(dir, './customizations.json'), {})
+            .then( (data) => {
+              const contents = JSON.parse(data);
+              this._customizations = contents.queries;
+              this._hiddenProps = contents.hiddenProps;
+            })
+            .catch((err) => {
+              console.log(`Failed to read saved toolbar customizations from file: ${err}`)
+            });
+        });
     }
 
     getCustomizations() {
